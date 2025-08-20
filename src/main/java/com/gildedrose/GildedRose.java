0c9240e33ea8274c964a6979b2338586f28210a3
hajer <hajer.ghorbal@gmail.com>
2025-08-20 17:07:17 +0200

    test

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.stream.Stream;
+
 public class GildedRose {
     Item[] items;
 
@@ -7,56 +9,50 @@ public class GildedRose {
         this.items = items;
     }
 
+    static boolean isNotLegendary(Item item) {
+        return "Sulfuras".equals(item.name);
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        Stream.of(items).filter(GildedRose::isNotLegendary).forEach(GildedRose::updateItemQ);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+    static void updateItemQ(Item item) {
+        if ("Backstage".equals(item.name) && canInc(item)) {
+            if (item.sellIn < 11 ) {
+                item.quality = item.quality + 2;
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (item.sellIn < 6) {
+                item.quality = item.quality + 3;
             }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+            if (item.sellIn < 1) {
+                item.quality = 0;
+            }
+            }
+            if ("Aged Brie".equals(item.name) && canInc(item)) {
+            if (item.sellIn < 51) {
+                item.quality++;
+            }
+            }
+        else
+            if (canDisc(item))
+            {  item.quality--;
+                if (item.sellIn<0 && canDisc(item)){
+                    item.quality--;
+                    if("Conjured".equals(item.name) && canDisc(item)){
+                        item.quality--;
                     }
                 }
             }
-        }
+
+
+
+    }
+    static boolean canInc(Item item) {
+        return item.quality <51;
+    }
+    static  boolean canDisc(Item item) {
+        return item.quality >0 && !"Aged Brie".equals( item.name );
     }
 }
